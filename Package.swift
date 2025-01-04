3d50c78d75959a8103f3005b4e67124b67dcfe9d
Lucas Gladding <lucasgladding@users.noreply.github.com>
2025-01-03 23:32:39 -0500

    Configured package.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Beat-and-Tempo-Tracking"),
+            name: "Beat-and-Tempo-Tracking",
+            path: "src",
+            publicHeadersPath: "."
+        ),
         .testTarget(
             name: "Beat-and-Tempo-TrackingTests",
             dependencies: ["Beat-and-Tempo-Tracking"]
