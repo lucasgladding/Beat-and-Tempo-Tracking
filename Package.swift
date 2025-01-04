e306b41aa7e3f0824c5d4c1d1967923fc27f6228
Lucas Gladding <lucasgladding@users.noreply.github.com>
2025-01-04 15:02:59 -0500

    Fixed build issues.

--- Package.swift
+++ Package.swift
@@ -6,22 +6,25 @@ import PackageDescription
 let package = Package(
     name: "Beat-and-Tempo-Tracking",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Beat-and-Tempo-Tracking",
             targets: ["Beat-and-Tempo-Tracking"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Beat-and-Tempo-Tracking",
-            path: "src",
-            publicHeadersPath: "."
-        ),
-        .testTarget(
-            name: "Beat-and-Tempo-TrackingTests",
-            dependencies: ["Beat-and-Tempo-Tracking"]
+            sources: [
+                "./BTT.c",
+                "./DFT.c",
+                "./fastsin.c",
+                "./Filter.c",
+                "./Statistics.c",
+                "./STFT.c",
+            ],
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath("./src")
+            ]
         ),
     ]
 )
--- Tests/Beat-and-Tempo-TrackingTests/Beat_and_Tempo_TrackingTests.swift
+++
@@ -1,6 +0,0 @@
-import Testing
-@testable import Beat_and_Tempo_Tracking
-
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
